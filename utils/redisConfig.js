7d2117b515e5f8af7d5c51cd87680e7f7a0ddd85
Tally-Client <inkdash6@gmail.com>
2025-04-24 02:11:46 +0200

    Merged feat_ems_qc

--- utils/redisConfig.js
+++ utils/redisConfig.js
@@ -2,16 +2,22 @@ const redis = require("redis");
 
 const redisClient = redis.createClient({
   socket: {
-    host: process.env.REDIS_HOST ,
-    port: process.env.REDIS_PORT 
-  }
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    tls: true 
+  },
+  password: process.env.REDIS_PASSWORD 
 });
 
 redisClient.on("connect", () => console.log("Connected to Redis..."));
 redisClient.on("error", (err) => console.error("Redis Connection Failed:", err));
 
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+  } catch (err) {
+    console.error("Failed to connect to Redis:", err);
+  }
 })();
 
 module.exports = redisClient;
